9fe7b57acc9a99bbdd19d9cc9c96a530d47318a9
Cherif <mdoucherifdiallo620@gmail.com>
2026-01-18 17:05:56 +0100

    ajouts de la structure de base

---
+++ src/main/java/org/example/gestiondeslieux/GestionDesLieuxApplication.java
@@ -0,0 +1,13 @@
+package org.example.gestiondeslieux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesLieuxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionDesLieuxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gestiondeslieux/GestionDesLieuxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gestiondeslieux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesLieuxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
